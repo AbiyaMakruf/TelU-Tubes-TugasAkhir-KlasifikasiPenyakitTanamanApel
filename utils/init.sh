6d1c51a314acee0fff1fd2f039bac98305d9c60f
AbiyaMakruf <aabbiiyyaa@gmail.com>
2024-09-29 12:55:02 +0000

    Edit: Mengubah init.sh. Edit: Menulis ulang notebook.ipynb

--- utils/init.sh
+++ utils/init.sh
@@ -8,4 +8,8 @@ apt-get update
 apt-get install -y nano
 git config --global user.email "aabbiiyyaa@gmail.com"
 git config --global user.name "AbiyaMakruf"
+pip install gdown
+pip install pandas
+pip install scikit-learn
+pip install scipy 
 echo "Inisialisasi Berhasil"
