a3cd20a8b8cdfcaf4fdcb8392d94edeb634834ea
AbiyaMakruf <aabbiiyyaa@gmail.com>
2024-09-29 08:30:18 +0000

    Edit: Mengubah script inisialisasi agar mengonfigurasi git config

--- utils/init.sh
+++ utils/init.sh
@@ -6,4 +6,6 @@
 echo "Memperbarui repositori paket..."
 apt-get update
 apt-get install -y nano
+git config --global user.email "aabbiiyyaa@gmail.com"
+git config --global user.name "AbiyaMakruf"
 echo "Inisialisasi Berhasil"
